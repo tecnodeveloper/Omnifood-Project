279bae4caa9ecff958c01554be5f4283679883d5
tecnodeveloper <recluzedev@gmail.com>
2025-04-30 21:56:11 +0500

    30 commit 5

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Omnifood Project
 
 ## Live Preview
+
+- The rendered size must be double then Intrinsic size in image view.
+- If the size of Intrinsic is less than double then change the width of image to it's double of rendered size.
--- js/script.js
+++ js/script.js
@@ -30,6 +30,37 @@ MobilebtnEl.addEventListener("click", function(){
 });
 
 ///////////////////////////////////////////////////////////
+// In future i will visit this code and understand it fully
+// Adding Smooth scrolling animation
+
+// const allLinks = document.querySelectorAll("a:link");
+
+// allLinks.forEach(function (link){
+//   link.addEventListener("click", function(e){
+//     e.preventDefault();
+//     const href = link.getAttribute("href");
+
+//     // Scroll back to top
+//     if(href === "#")
+//     window.scrollTo({
+//   top:0,
+// behavior: "smooth",});
+// //Scroll to other links
+// if(href != "#" && href.startsWith("#"))
+// {
+//   const sectionEl = document.querySelector(href);
+//   sectionEl.scrollIntoView({behavior: smooth});
+// }
+
+// Close Mobile Navigation
+// if(link.classList.contains("main-nav-link"))
+//   headerEl.classList.toggle("nav-open")
+//   }
+// )
+
+// });
+
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
